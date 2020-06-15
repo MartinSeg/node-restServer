17f99466633c8c365eeca4acdd27a6827e321cfa
Martin Seghezzo <martinseghezzo@gmail.com>
2020-06-15 17:22:12 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else{ 
-    urlDB = 'mongodb+srv://taios:pgSrnNYUxlKN5a8M@cluster0-6x22j.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
